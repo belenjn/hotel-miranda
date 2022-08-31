dbc93ba097fa38b0ae266a0e88cfe82f6777c7e3
Belen JN <belenjarabanavas31@gmail.com>
2022-08-31 12:57:38 +0200

    Bullets with an error

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,22 +1,39 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    autoplay: {
-      delay: 4000,
-    },
-  
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      enabled: false
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
-  
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    enabled: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper2 = new Swiper(".swiper__facilities", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    dynamicMainBullets: 4,
+    clickable: true,
+    enabled: true,
+  }
+});
