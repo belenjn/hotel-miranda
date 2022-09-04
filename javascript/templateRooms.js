538b040b4376e362efc6880cd689cc455de4cc00
Belen JN <belenjarabanavas31@gmail.com>
2022-09-04 11:53:30 +0200

    Creating template for rooms

--- javascript/templateRooms.js
+++ javascript/templateRooms.js
@@ -1,33 +1,15 @@
+// const API_URL = "http://localhost:3000";
 
-// const API_URL = "https://hotelmirandaapi.azurewebsites.net";
+// const roomsDiv = document.querySelector(".rooms");
+// const ul = document.createElement("div");
 
-// const rooms = document.querySelector(".rooms");
-
-// export const apiRequest = async () => {
-//   try {
-//     const response = await fetch(API_URL + "/rooms", {
-//       method: "GET",
-//       headers: {
-//         "Access-Control-Allow-Origin": `${API_URL}`,
-//         "Access-Control-Allow-Credentials": true
-//       },
+// fetch(`${API_URL}/rooms`)
+//   .then((response) => response.json())
+//   .then((rooms) => {
+//     rooms.forEach((room) => {
+//       let li = document.createElement("div");
+//       li.appendChild(document.createTextNode(`${room.id}`));
+//       ul.appendChild(li);
 //     });
-//     if (response.ok) {
-//       const jsonResponse = await response.json();
-//       console.log(jsonResponse)
-//       return jsonResponse;
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// apiRequest()
-
-// // const template = rooms.map((room) => {
-// //   `<li>
-// //   <h3>${room.bed_type}</h3>
-// //   <h3>${room.rate}<small>/Night</small></h3>
-// //   </li>`;
-// // });
-// // rooms.innerHTML = `<ul>${template}</ul>`
+//     roomsDiv.appendChild(ul);
+//   });
