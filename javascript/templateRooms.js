451da1f61d0f81d1e56edca0ca9ef86e46523f83
Belen JN <belenjarabanavas31@gmail.com>
2022-09-04 10:44:17 +0200

    Fake data added

--- javascript/templateRooms.js
+++ javascript/templateRooms.js
@@ -1,22 +1,33 @@
-const { default: fetch } = require("cross-fetch");
 
+// const API_URL = "https://hotelmirandaapi.azurewebsites.net";
 
-const API_URL = "https://hotelmirandaapi.azurewebsites.net";
+// const rooms = document.querySelector(".rooms");
 
-const rooms = document.querySelector(".rooms");
+// export const apiRequest = async () => {
+//   try {
+//     const response = await fetch(API_URL + "/rooms", {
+//       method: "GET",
+//       headers: {
+//         "Access-Control-Allow-Origin": `${API_URL}`,
+//         "Access-Control-Allow-Credentials": true
+//       },
+//     });
+//     if (response.ok) {
+//       const jsonResponse = await response.json();
+//       console.log(jsonResponse)
+//       return jsonResponse;
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-fetch(`${API_URL}/rooms`)
-  .then((response) => response.json())
+// apiRequest()
 
-  .then((rooms) => {
-    console.log(rooms)
-
-
-    // const template = rooms.map((room) => {
-    //   `<li>
-    //   <h3>${room.bed_type}</h3> 
-    //   <h3>${room.rate}<small>/Night</small></h3>
-    //   </li>`;
-    // });
-    // rooms.innerHTML = `<ul>${template}</ul>`
-  });
+// // const template = rooms.map((room) => {
+// //   `<li>
+// //   <h3>${room.bed_type}</h3>
+// //   <h3>${room.rate}<small>/Night</small></h3>
+// //   </li>`;
+// // });
+// // rooms.innerHTML = `<ul>${template}</ul>`
