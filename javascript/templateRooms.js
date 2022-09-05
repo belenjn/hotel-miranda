0bf90f2362ec1111d194edf83f40b6fa9e334903
Belen JN <belenjarabanavas31@gmail.com>
2022-09-05 12:53:57 +0200

    added images in db.json and rooms cards in room pg

--- javascript/templateRooms.js
+++ javascript/templateRooms.js
@@ -1,15 +1,47 @@
-// const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:3000";
 
-// const roomsDiv = document.querySelector(".rooms");
-// const ul = document.createElement("div");
+const roomsDiv = document.querySelector(".rooms");
+const description = document.querySelector(".description");
+const rate = document.querySelector(".rate");
 
-// fetch(`${API_URL}/rooms`)
-//   .then((response) => response.json())
-//   .then((rooms) => {
-//     rooms.forEach((room) => {
-//       let li = document.createElement("div");
-//       li.appendChild(document.createTextNode(`${room.id}`));
-//       ul.appendChild(li);
-//     });
-//     roomsDiv.appendChild(ul);
-//   });
+fetch(`${API_URL}/rooms`)
+  .then((response) => response.json())
+  .then((rooms) => {
+    rooms.map((room) => {
+
+      let divSingleRoom = document.createElement("div");
+
+      let title = document.createElement("h1");
+
+      let description = document.createElement("p");
+
+      let rate = document.createElement("h5");
+
+      let image = document.createElement("img");
+
+      let button = document.createElement("button");
+
+      title.appendChild(document.createTextNode(`${room.bed_type}`));
+
+      description.appendChild(document.createTextNode(`${room.description}`));
+
+      rate.appendChild(document.createTextNode(`$${room.rate}/Night`));
+
+      button.appendChild(document.createTextNode(`Booking now`));
+
+      image.src = `${room.image}`
+      
+      divSingleRoom.appendChild(image);
+
+      divSingleRoom.appendChild(title);
+
+      divSingleRoom.appendChild(description);
+
+      divSingleRoom.appendChild(rate);
+
+      // divSingleRoom.appendChild(button);
+
+
+      roomsDiv.appendChild(divSingleRoom);
+    });
+  });
