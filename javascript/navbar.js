9b2b2db3ee901f7e81f204af2a565f313e48cd37
Belen JN <belenjarabanavas31@gmail.com>
2022-10-19 09:15:51 +0200

    New styles added in navbar's search input

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -20,14 +20,21 @@ closeButton.addEventListener("click", function () {
 const takeATourButton = document.querySelector("#tour__button");
 
 takeATourButton.addEventListener("click", function () {
-
   window.scrollTo("2400", "1300");
-
 });
 
 const learnButton = document.querySelector("#learn__button");
 
-learnButton.addEventListener("click", function() {
-  window.scrollTo("3000", "2500")
-})
+learnButton.addEventListener("click", function () {
+  window.scrollTo("3000", "2500");
+});
 
+const searchButtonNavbar = document.querySelector(".navbar__search");
+const inputNavbar = document.querySelector(".search__hidden");
+
+searchButtonNavbar.addEventListener("click", function () {
+  inputNavbar.classList.remove("search__hidden");
+  searchButtonNavbar.classList.remove("navbar__search");
+  inputNavbar.classList.add("search__visible");
+  searchButtonNavbar.classList.add("search__hidden");
+});
