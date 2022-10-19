9fd394cc81518226ed7f142bb021524671c9ddbd
Belen JN <belenjarabanavas31@gmail.com>
2022-10-19 09:40:09 +0200

    New functions for buttons

---
+++ javascript/home.js
@@ -0,0 +1,37 @@
+const takeATourButton = document.querySelector("#tour__button");
+
+takeATourButton.addEventListener("click", function () {
+  window.scrollTo("2400", "1300");
+});
+
+const learnButton = document.querySelector("#learn__button");
+
+learnButton.addEventListener("click", function () {
+  window.scrollTo("3000", "2500");
+});
+
+const searchButtonNavbar = document.querySelector(".navbar__search");
+const inputNavbar = document.querySelector(".search__hidden");
+
+searchButtonNavbar.addEventListener("click", function () {
+  inputNavbar.classList.remove("search__hidden");
+  searchButtonNavbar.classList.remove("navbar__search");
+  inputNavbar.classList.add("search__visible");
+  searchButtonNavbar.classList.add("search__hidden");
+});
+
+inputNavbar.addEventListener("change", function (e) {
+  e.target.value ? (window.location.href = "rooms.html") : "";
+});
+
+const button = document.querySelector("#button");
+
+button.addEventListener("click", function () {
+  window.location.href = "rooms.html";
+});
+
+const buttonVideo = document.querySelector("#button__video");
+
+buttonVideo.addEventListener("click", function () {
+  window.location.href = "rooms.html";
+});
--- javascript/navbar.js
+++ javascript/navbar.js
@@ -17,24 +17,3 @@ closeButton.addEventListener("click", function () {
   container.classList.remove("navbar__categories--mobile-hidden");
 });
 
-const takeATourButton = document.querySelector("#tour__button");
-
-takeATourButton.addEventListener("click", function () {
-  window.scrollTo("2400", "1300");
-});
-
-const learnButton = document.querySelector("#learn__button");
-
-learnButton.addEventListener("click", function () {
-  window.scrollTo("3000", "2500");
-});
-
-const searchButtonNavbar = document.querySelector(".navbar__search");
-const inputNavbar = document.querySelector(".search__hidden");
-
-searchButtonNavbar.addEventListener("click", function () {
-  inputNavbar.classList.remove("search__hidden");
-  searchButtonNavbar.classList.remove("navbar__search");
-  inputNavbar.classList.add("search__visible");
-  searchButtonNavbar.classList.add("search__hidden");
-});
