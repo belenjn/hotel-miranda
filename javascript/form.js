c8433b9db894e5f5ba97760337e6d4356889c51a
Belen JN <belenjarabanavas31@gmail.com>
2022-11-17 16:54:04 +0100

    Form done

--- javascript/form.js
+++ javascript/form.js
@@ -1,3 +1,23 @@
-const submitButton = document.getElementsByClassName("button");
+const submitButton = document.getElementsByClassName("button")[0];
 
-console.log(submitButton)
+const firstName = document.getElementsByClassName("fname")[0];
+const lastName = document.getElementsByClassName("lname")[0];
+const subject = document.getElementsByClassName("subject")[0];
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (firstName.value === "" || lastName.value === "" || subject.value === "")
+    swal("Please complete all fields in the form", "", "error");
+  else {
+    swal("Your request has been processed successfully", "", "success").then(
+      (result) => {
+        if (result) {
+          firstName.value = "";
+          lastName.value = "";
+          subject.value = "";
+        }
+      }
+    );
+  }
+});
